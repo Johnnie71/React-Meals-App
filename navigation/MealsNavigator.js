a522f4f777d9068c058349b6edd8efde2ce46235
johnnie71 <johnnie.agonz71@gmail.com>
2021-01-27 19:08:13 -0500

    added screens to stack navigator

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,3 +1,10 @@
 import { createStackNavigator} from 'react-navigation-stack';
 
+import CatgoriesScreen from '../screens/CategoriesScreen';
+import CategoriesMealScreen from '../screens/CategoryMealsScreen';
+import CategoryMealsScreen from '../screens/CategoryMealsScreen';
 
+createStackNavigator({
+    Categories: CatgoriesScreen,
+    CategoryMeals: CategoriesMealScreen,
+});

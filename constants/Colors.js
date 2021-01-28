7206d327873a36c2c28eae2c35ae34aa791f3370
johnnie71 <johnnie.agonz71@gmail.com>
2021-01-28 15:30:03 -0500

    added primary and accent colors

---
+++ constants/Colors.js
@@ -0,0 +1,4 @@
+export default {
+    primaryColor: '#FF7700',
+    accentColor: '#86F30A'
+};
--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { View, Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
 
 import { CATEGORIES }from '../data/dummy-data';
+import Colors from '../constants/Colors'
 
 
 
@@ -9,14 +10,24 @@ const CategoriesScreen = props => {
 
     const renderGridItem = (itemData) => {
         return (
-            <TouchableOpacity onPress={() => {
-                props.navigation.navigate();
-            }}>
-                <View style={styles.gridItem}>
-                    <Text>{itemData.item.title}</Text>
-                </View>
+            <TouchableOpacity
+                style={styles.gridItem}
+                onPress={() => {
+                props.navigation.navigate('CategoryMeals');
+                }}>
+              <View >
+                 <Text>{itemData.item.title}</Text>
+              </View>
             </TouchableOpacity>
         );
+    };
+
+    CategoriesScreen.navigationOptions = {
+        headerTitle: 'Meal Categories',
+        headerStyle: {
+            backgroundColor: Colors.primaryColor ,
+        },
+        headerTintColor: 'white' ,
     };
 
     return (

e16ab9c5ab5c13662c8b7657e9aa2ecffdc8c30d
johnnie71 <johnnie.agonz71@gmail.com>
2021-01-27 19:38:40 -0500

    added onpress navigation to Category Meals

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -2,10 +2,14 @@ import React from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 
 const CategoriesScreen = props => {
+
     return (
         <View style={styles.screen}>
             <Text>The Categories Screen!</Text>
-            <Button title="Go To Meals!" />
+            <Button title="Go To Meals!" onPress={() => {
+                props.navigation.navigate("CategoryMeals");
+            }}
+            />
         </View>
     );
 };

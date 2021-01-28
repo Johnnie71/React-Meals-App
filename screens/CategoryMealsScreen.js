56b818f8deabc4a33932ba10466ee0437fee5241
johnnie71 <johnnie.agonz71@gmail.com>
2021-01-28 15:41:38 -0500

    used catId to get the categort ID and .find to find that specific category

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 
+import { CATEGORIES } from '../data/dummy-data';
+
 const CategoriesMealScreen = props => {
+
+    const catId = props.navigation.getParam('categoryId');
+
+    const selectedCategories = CATEGORIES.find(cat => cat.id === catId);
+
     return (
         <View style={styles.screen}>
             <Text>The Category Meal Screen!</Text>
-            <Button title="Go To Meal Detail " onPress={() => {
+            <Text>{selectedCategories.title}</Text>
+            <Button title="Go To Details " onPress={() => {
                 props.navigation.navigate('MealDetail')
             }}/>
             <Button title="Go Back" onPress={() => {

478bea6761fc2d7ef1cab9067e36930ef492eeaa
johnnie71 <johnnie.agonz71@gmail.com>
2021-02-01 20:06:54 -0500

    added headerButton and installed expo vector icons

---
+++ components/HeaderButton.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { HeaderButton } from 'react-navigation-header-buttons';
+import {} from '@expo/vector-icons'
+
+const HeaderButton = props => {
+
+
+};
+
+export default HeaderButton;

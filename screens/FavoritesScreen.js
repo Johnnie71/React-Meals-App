8da81ed4137c9c13e1d2605e3823369bd7b00728
johnnie71 <johnnie.agonz71@gmail.com>
2021-02-03 09:37:06 -0500

    got rid of styles and imports from react native

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
 import MealList from '../components/MealList';
 
 const FavoritesScreen = props => {
@@ -10,12 +9,5 @@ FavoritesScreen.navigationOptions = {
     headerTitle: "Your Favorites"
 };
 
-const styles = StyleSheet.create({
-    screen:{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
-});
 
 export default FavoritesScreen;

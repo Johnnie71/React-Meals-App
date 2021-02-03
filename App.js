c39212cfe9d91ad5e6f1ff572269fa3fa01286e7
johnnie71 <johnnie.agonz71@gmail.com>
2021-02-02 19:44:48 -0500

    removed ioncons

--- App.js
+++ App.js
@@ -3,7 +3,6 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import * as Font from 'expo-font';
 import AppLoading from 'expo-app-loading';
-import { Ionicons } from '@expo/vector-icons';
 
 import MealsNavigator from './navigation/MealsNavigator';
 import  { enableScreens } from 'react-native-screens';
@@ -14,7 +13,6 @@ const fetchFonts = () => {
    return Font.loadAsync({
     'open-sans': require('./assets/fonts/OpenSans_Regular.ttf'),
     'open-sans-bold': require('./assets/fonts/OpenSans_Bold.ttf'),
-    //  Ionicons: require('@expo/vector-icons/fonts/Ionicons.ttf'),
   });
 };
 

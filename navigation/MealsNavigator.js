71ec1cc358416f6e17be4bf304a778f9d9112970
johnnie71 <johnnie.agonz71@gmail.com>
2021-02-02 20:59:59 -0500

    added platform

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -3,12 +3,14 @@ import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { Ionicons } from '@expo/vector-icons';
+import { Platform } from 'react-native';
 
 import CatgoriesScreen from '../screens/CategoriesScreen';
 import CategoryMealsScreen from '../screens/CategoryMealsScreen';
 import MealDetailScreen from '../screens/MealDetailScreen';
 import FavoritesScreen from '../screens/FavoritesScreen';
 import Colors from '../constants/Colors';
+import { createMaterialBottomTabNavigator } from 'react-native-materials';
 
 
 const MealsNavigator = createStackNavigator({

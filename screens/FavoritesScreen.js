01b805dc6103b8732efd20d1403ca1714f34b03f
johnnie71 <johnnie.agonz71@gmail.com>
2021-02-04 10:35:18 -0500

    imported buttons item and headerbutton

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import MealList from '../components/MealList';
 import { MEALS } from '../data/dummy-data';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import HeaderButton from '../components/HeaderButton';
 
 const FavoritesScreen = props => {
 

e807a628fcbbb7ca193addd9342aef22d096f100
johnnie71 <johnnie.agonz71@gmail.com>
2021-02-05 18:50:35 -0500

    imported scrollview

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { ScrollView, View, Text, StyleSheet, Button } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 import { MEALS } from '../data/dummy-data';

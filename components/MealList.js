412f342628d042673ffd92717e91bceebb0c5d5b
johnnie71 <johnnie.agonz71@gmail.com>
2021-02-07 16:42:47 -0500

    deleted text import

--- components/MealList.js
+++ components/MealList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet, FlatList, Text} from 'react-native';
+import { View, StyleSheet, FlatList } from 'react-native';
 import MealItem from './MealItem';
 
 const MealList = props => {

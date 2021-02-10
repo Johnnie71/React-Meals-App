3b56dc944c2dcaeffad91df7594c01c86bd16f4c
johnnie71 <johnnie.agonz71@gmail.com>
2021-02-10 16:39:21 -0500

    imported useSelector hook

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import MealList from '../components/MealList';
-import { MEALS } from '../data/dummy-data';
+import { useSelector } from 'react-redux';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import HeaderButton from '../components/HeaderButton';
 

335363bdbb9bdb5cf95cb65c8974c4a7409fff4e
johnnie71 <johnnie.agonz71@gmail.com>
2021-02-11 15:07:18 -0500

    added toggleFavorite to toggleHandler to transfer mealId

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -25,7 +25,7 @@ const MealDetailScreen = props => {
     const dispatch = useDispatch();
 
    const toggleFavoriteHandler = () => {
-       dispatch()
+       dispatch(toggleFavorite(mealId))
    }
 
     // useEffect(() => {

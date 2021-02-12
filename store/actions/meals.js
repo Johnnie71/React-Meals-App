b72143e9b0855011359a18157e9abb4d13ecd322
johnnie71 <johnnie.agonz71@gmail.com>
2021-02-12 15:11:46 -0500

    exported as variable to use

--- store/actions/meals.js
+++ store/actions/meals.js
@@ -1,5 +1,7 @@
 export const TOGGLE_FAVORITE = 'TOGGLE_FAVORITE';
+export const SET_FILTERS = 'SET_FILTERS';
 
 export const toggleFavorite = (id) => {
     return { type: TOGGLE_FAVORITE, mealId: id };
-}
+};
+

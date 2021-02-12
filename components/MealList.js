68a6e21e868d1ef6ad3c18685443a1c6a0edaa5a
johnnie71 <johnnie.agonz71@gmail.com>
2021-02-12 14:58:54 -0500

    added boolean is favorite to navigation params

--- components/MealList.js
+++ components/MealList.js
@@ -22,7 +22,7 @@ const MealList = props => {
                         routeName: 'MealDetail', params: {
                            mealId: itemData.item.id,
                            mealTitle: itemData.item.title,
-                           isFav: 
+                           isFav: isFavorite
                     }});
                 }}
            />

b2621eb8498081fbc006ac3a939a5b4a8f834952
johnnie71 <johnnie.agonz71@gmail.com>
2021-02-12 15:01:51 -0500

    added if statement as a fallback if there are no favorited meals

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -3,10 +3,19 @@ import MealList from '../components/MealList';
 import { useSelector } from 'react-redux';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import HeaderButton from '../components/HeaderButton';
+import { View } from 'react-native';
 
 const FavoritesScreen = props => {
 
     const favMeals = useSelector(state => state.meals.favoriteMeals);
+
+    if(favMeals.length === 0 || !favMeals){
+        return (
+            <View>
+                <Text>No favorite meals found. Start adding some!</Text>
+            </View>
+        )
+    }
 
     
     return <MealList listData={favMeals} navigation={props.navigation} />
